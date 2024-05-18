a944bf8769d0331d2ca700dbf8d6de23f8cf31b8
Sergey J <wint@yandex-team.ru>
2024-05-18 18:28:30 +0300

    add w sign

--- internal/cryptandsign/cryptandsign.go
+++ internal/cryptandsign/cryptandsign.go
@@ -5,6 +5,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"hash"
 	"io"
 	"log"
 	"net/http"
@@ -14,42 +15,90 @@ import (
 
 const signHeaderType = "HashSHA256"
 
-type AgentSendFunc func(r *resty.Request, send, serverHost string) (*resty.Response, error)
+type (
+	AgentSendFunc func(r *resty.Request, send, serverHost string) (*resty.Response, error)
+
+	responseData struct {
+		respCode int
+	}
+
+	signResponseWriter struct {
+		wr              http.ResponseWriter
+		respData        *responseData
+		sign            hash.Hash
+		headersSetDone  chan bool
+		headersSendDone chan bool
+	}
+)
+
+func (s *signResponseWriter) Write(b []byte) (int, error) {
+	s.sign.Write(b)
+	res := s.sign.Sum(nil)
+	s.wr.Header().Set(signHeaderType, hex.EncodeToString(res))
+	s.headersSetDone <- true
+	<-s.headersSendDone
+	size, err := s.wr.Write(b)
+	return size, err
+}
+
+func (s *signResponseWriter) WriteHeader(statusCode int) {
+	go func() {
+		<-s.headersSetDone
+		s.wr.WriteHeader(statusCode)
+		s.headersSendDone <- true
+	}()
+	s.respData.respCode = statusCode
+}
+
+func (s *signResponseWriter) Header() http.Header {
+	return s.wr.Header()
+}
 
 func SignCheck(h http.HandlerFunc, seckey string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		hashSignStr := r.Header.Get(signHeaderType)
-		if seckey == "" || hashSignStr == "" {
+		if seckey == "" {
 			h(w, r)
 			return
 		}
 
-		// for hmac
-		req, err := io.ReadAll(r.Body)
-		if err != nil {
-			// tmp
-			log.Println("sign: error read request body")
-			http.Error(w, "error read request body", http.StatusBadRequest)
-			return
-		}
-		r.Body = io.NopCloser(bytes.NewBuffer(req))
-		hashSign, err := hex.DecodeString(hashSignStr)
-		if err != nil {
-			// tmp
-			log.Println("sign: can't decode hashSign")
-			http.Error(w, "can't decode hashSign", http.StatusBadRequest)
-			return
-		}
 		hm := hmac.New(sha256.New, []byte(seckey))
-		hm.Write(req)
-		reshm := hm.Sum(nil)
-		if hmac.Equal(hashSign, reshm) {
-			h(w, r)
+
+		rdata := &responseData{}
+		cw := &signResponseWriter{
+			wr:              w,
+			respData:        rdata,
+			sign:            hm,
+			headersSetDone:  make(chan bool, 1),
+			headersSendDone: make(chan bool, 1)}
+
+		// for hmac
+		if hashSignStr != "" {
+			req, err := io.ReadAll(r.Body)
+			if err != nil {
+				log.Println("sign: error read request body")
+				http.Error(w, "error read request body", http.StatusBadRequest)
+				return
+			}
+			r.Body = io.NopCloser(bytes.NewBuffer(req))
+			defer r.Body.Close()
+			hashSign, err := hex.DecodeString(hashSignStr)
+			if err != nil {
+				log.Println("sign: can't decode hashSign")
+				http.Error(w, "can't decode hashSign", http.StatusBadRequest)
+				return
+			}
+			hm.Write(req)
+			reshm := hm.Sum(nil)
+			if hmac.Equal(hashSign, reshm) {
+				h(cw, r)
+				return
+			}
+			log.Println("sign: sign error")
+			http.Error(w, "sign error", http.StatusBadRequest)
 			return
 		}
-		// tmp
-		log.Println("sign: sign error")
-		http.Error(w, "sign error", http.StatusBadRequest)
+		h(cw, r)
 	}
 }
 
