55cffe35675d3e83ab18c0ceac1e7c495d0d2409
Sergey J <wint@yandex-team.ru>
2024-07-15 21:44:15 +0300

    add compress doc

--- internal/compression/compression.go
+++ internal/compression/compression.go
@@ -1,3 +1,4 @@
+// Package for compression HTTP data.
 package compression
 
 import (
@@ -7,13 +8,16 @@ import (
 	"strings"
 )
 
+// Allowed compression content types.
 const allowedCompressTypes = "text/html application/json"
 
+// Middleware compress writer type.
 type compressWriter struct {
 	w  http.ResponseWriter
 	zw *gzip.Writer
 }
 
+// Init new compress writer for HTTP response.
 func newCompressWriter(w http.ResponseWriter) *compressWriter {
 	return &compressWriter{
 		w:  w,
@@ -21,28 +25,34 @@ func newCompressWriter(w http.ResponseWriter) *compressWriter {
 	}
 }
 
+// Middleware get Header method.
 func (c *compressWriter) Header() http.Header {
 	return c.w.Header()
 }
 
+// Middleware Write method.
 func (c *compressWriter) Write(p []byte) (int, error) {
 	return c.zw.Write(p)
 }
 
+// Middleware WriteHeader method.
 func (c *compressWriter) WriteHeader(statusCode int) {
 	c.w.Header().Set("Content-Encoding", "gzip")
 	c.w.WriteHeader(statusCode)
 }
 
+// Close compress stream (writer).
 func (c *compressWriter) Close() error {
 	return c.zw.Close()
 }
 
+// Middleware compression reader type.
 type compressReader struct {
 	r  io.ReadCloser
 	zr *gzip.Reader
 }
 
+// Init new compress reader for HTTP requests.
 func newCompressReader(r io.ReadCloser) (*compressReader, error) {
 	zr, err := gzip.NewReader(r)
 	if err != nil {
@@ -55,10 +65,12 @@ func newCompressReader(r io.ReadCloser) (*compressReader, error) {
 	}, nil
 }
 
+// Middleware Read method.
 func (c compressReader) Read(p []byte) (n int, err error) {
 	return c.zr.Read(p)
 }
 
+// Close compress stream (reader).
 func (c *compressReader) Close() error {
 	if err := c.r.Close(); err != nil {
 		return err
@@ -66,6 +78,7 @@ func (c *compressReader) Close() error {
 	return c.zr.Close()
 }
 
+// Main function for compress/decompress HTTP requests/response.
 func GzipCompDecomp(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
