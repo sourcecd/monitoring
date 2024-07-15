47370accac11c7a2b7d9e98e221a879428188770
Sergey J <wint@yandex-team.ru>
2024-07-15 17:27:00 +0300

    add logs doc

--- internal/metrictypes/metrictypes.go
+++ internal/metrictypes/metrictypes.go
@@ -1,11 +1,16 @@
+// Package with metric types.
 package metrictypes
 
 const (
+	// Gauge.
 	GaugeType   = "gauge"
+	// Counter.
 	CounterType = "counter"
 )
 
 type (
+	// Base type for gauge metric - float64
 	Gauge   float64
+	// Base type for counter metric - int64
 	Counter int64
 )
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,8 +1,10 @@
+// Metrics model (struct).
 package models
 
+// Type of metrics model.
 type Metrics struct {
-	ID    string   `json:"id"`              // имя метрики
-	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
-	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
-	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+	ID    string   `json:"id"`              // metric name
+	MType string   `json:"type"`            // parameter, recives value gauge or counter
+	Delta *int64   `json:"delta,omitempty"` // metric value if counter received
+	Value *float64 `json:"value,omitempty"` // metric value if gauge received
 }
