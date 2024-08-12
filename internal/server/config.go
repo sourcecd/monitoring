730a67786a7cfa647fa61f568f6dbaf97b10929e
Sergey J <wint@yandex-team.ru>
2024-08-12 22:42:08 +0300

    add json tags to cfg

--- internal/server/config.go
+++ internal/server/config.go
@@ -2,13 +2,13 @@ package server
 
 // ConfigArgs stores server config information.
 type ConfigArgs struct {
-	DatabaseDsn     string // database connection string
-	PprofAddr       string // address for pprof buildin server
-	KeyEnc          string // symmetric encryption key for signing requests
-	ServerAddr      string // server address
-	Loglevel        string // level of logging
-	FileStoragePath string // path to file, where metrics will be store
-	PrivKeyFile     string // path to private key file for asymmetric encryption
-	StoreInterval   int    // periodic interval before save metrics data to file
-	Restore         bool   // a flag that indicates whether to restore saved metrics from a file when starting the server
+	DatabaseDsn     string `json:"database_dsn"`   // database connection string
+	PprofAddr       string `json:"pprof_address"`  // address for pprof buildin server
+	KeyEnc          string `json:"key_enc_sign"`   // symmetric encryption key for signing requests
+	ServerAddr      string `json:"address"`        // server address
+	Loglevel        string `json:"log_level"`      // level of logging
+	FileStoragePath string `json:"store_file"`     // path to file, where metrics will be store
+	PrivKeyFile     string `json:"crypto_key"`     // path to private key file for asymmetric encryption
+	StoreInterval   int    `json:"store_interval"` // periodic interval before save metrics data to file
+	Restore         bool   `json:"restore"`        // a flag that indicates whether to restore saved metrics from a file when starting the server
 }
